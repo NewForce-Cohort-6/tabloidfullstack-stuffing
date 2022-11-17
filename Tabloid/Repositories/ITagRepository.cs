74706e6645160b570d9209cc6244fe0a8f35309f
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-11-17 11:28:19 -0500

    Creates tags folder, TagList.js, TagNew.js, Models>Tag.cs, Repos>TagRepository.cs and ITagRepository.cs, Controllers>TagController.cs. Changes Tabloid>Startup.cs, Tabloid>Client>src>nav>Header.js

---
+++ Tabloid/Repositories/ITagRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Tabloid.Models;
+
+namespace Tabloid.Repositories
+{
+    public interface ITagRepository
+    {
+        void AddTag(Tag tag);
+        List<Tag> GetAll();
+    }
+}
---
+++ Tabloid/Repositories/TagRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using Tabloid.Models;
+using Tabloid.Utils;
+
+namespace Tabloid.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration configuration) : base(configuration) { }
+
+        //Get a list of all tags
+        public List<Tag> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name FROM Tag ORDER BY Name";
+                    var reader = cmd.ExecuteReader();
+
+                    List<Tag> tags = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        tags.Add(new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name"))
+                        });
+                    }
+                    reader.Close();
+                    return tags;
+                }
+            }
+        }
+
+        //Allow (admin) users to create tags. TODO: limit to admin.
+        public void AddTag(Tag tag)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                    INSERT INTO Tag(Name)
+                                    OUTPUT INSERTED.ID
+                                    VALUES (@name)";
+                    cmd.Parameters.AddWithValue("@name", tag.Name);
+                    tag.Id = (int)cmd.ExecuteScalar();
+                }
+
+            }
+        }
+    }
+}
