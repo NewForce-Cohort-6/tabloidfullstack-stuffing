5a49b4209926bc776969d209f7b65daf7c3b0ec5
shanedbutler <shanedbutler@gmail.com>
2022-11-16 19:25:15 -0500

    Create sub-folders and place related components

--- Tabloid/client/src/components/views/ApplicationViews.js
+++ Tabloid/client/src/components/views/ApplicationViews.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Hello from "./Hello";
+import Hello from "../Hello";
 
 export default function ApplicationViews() {
 
