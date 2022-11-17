7af596c9e495161b64fdc617c1e586ddc4c7008e
shanedbutler <shanedbutler@gmail.com>
2022-11-17 13:10:02 -0500

    Edit sql query for get all post to be sorted by publish time descending

--- Tabloid/Repositories/PostRepository.cs
+++ Tabloid/Repositories/PostRepository.cs
@@ -32,7 +32,8 @@ namespace Tabloid.Repositories
                               LEFT JOIN Category c ON p.CategoryId = c.id
                               LEFT JOIN UserProfile u ON p.UserProfileId = u.id
                               LEFT JOIN UserType ut ON u.UserTypeId = ut.id
-                        WHERE IsApproved = 1 AND PublishDateTime < SYSDATETIME()";
+                        WHERE IsApproved = 1 AND PublishDateTime < SYSDATETIME()
+                        ORDER BY p.PublishDateTime DESC";
                     var reader = cmd.ExecuteReader();
 
                     var posts = new List<Post>();
