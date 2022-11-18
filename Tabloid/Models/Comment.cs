1e3d356f9e181e85f01bd24b224f1689e878cedd
Toni Baker <tonijbaker5@gmail.com>
2022-11-18 14:09:35 -0500

    comment.cs

--- Tabloid/Models/Comment.cs
+++ Tabloid/Models/Comment.cs
@@ -1,6 +1,15 @@
-﻿namespace Tabloid.Models
+﻿using System;
+using System.Web;
+
+namespace Tabloid.Models
 {
     public class Comment
     {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public int UserProfileId { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
     }
 }
