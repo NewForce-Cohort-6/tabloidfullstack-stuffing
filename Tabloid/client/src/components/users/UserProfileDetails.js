1595aa879bb146c9a7e19f2e6dc8c91504ed26cb
Toni Baker <tonijbaker5@gmail.com>
2022-11-18 12:38:50 -0500

    user profile card reactstrap change

--- Tabloid/client/src/components/users/UserProfileDetails.js
+++ Tabloid/client/src/components/users/UserProfileDetails.js
@@ -3,6 +3,7 @@ import { getAllUsers, getSingleUser } from "../../Managers/UserProfileManager";
 import { UserProfileItem } from "./UserProfileItem";
 import { Table } from "reactstrap";
 import {Link, useParams} from "react-router-dom";
+import { Card, CardBody, CardLink, CardText, CardTitle, ListGroup, ListGroupItem } from "reactstrap";
 
 
 
@@ -11,6 +12,11 @@ export default function UserProfileDetails (){
   const [userProfile, setUserProfile] = useState({});
 
   const {id} = useParams();
+  
+  const handleBrokenImage = (image) => {
+    const defaultImage = "https://contenthub-static.grammarly.com/blog/wp-content/uploads/2017/11/how-to-write-a-blog-post.jpeg";
+    image.target.src = defaultImage;
+}
   
   const getSingleUserProfile = () =>{
     getSingleUser(id).then(singleUser => setUserProfile(singleUser));
@@ -21,41 +27,38 @@ export default function UserProfileDetails (){
     }, []); 
   
   return (
-    <>
-    <Link to="/users">Back</Link>
-    <Table>
-      <thead>
-        <tr>
-          <th>Full Name</th>
-          <th>Image</th>
-          <th>Display Name</th>
-          <th>Email</th>
-          <th>Profile Created</th>
-          <th>User Type</th>
-        </tr>
-      </thead>
-      <tbody>
-      <tr>
-            <th scope="row">
+    
+    <section className="m-5">
+    <Card
+        style={{
+            width: '18rem'
+        }}
+    >
+        <img
+            alt="Post"
+            src={userProfile.imageLocation}
+            onError={handleBrokenImage}
+        />
+        <CardBody>
+            <CardTitle tag="h5">
                 {userProfile.fullName}
-            </th>
-            <td>
-                {userProfile.imageLocation}
-            </td>
-            <td>
-                {userProfile.displayName}
-            </td>
-            <td>
-                {userProfile.email}
-            </td>
-            <td>
-                {userProfile.createDateTime}
-            </td>
-            <td>
-                {userProfile?.userType?.name}
-            </td>
-        </tr>
-      </tbody>
-    </Table></>
+            </CardTitle>
+        </CardBody>
+        <ListGroup flush>
+            <ListGroupItem>
+                Display Name:<br/> {userProfile.displayName}
+            </ListGroupItem>
+            <ListGroupItem>
+                Email:<br/> {userProfile.email}
+            </ListGroupItem>
+            <ListGroupItem>
+                User Creation Date:<br/> {userProfile.createDateTime}
+            </ListGroupItem>
+            <ListGroupItem>
+                User Type:<br/> {userProfile?.userType?.name}
+            </ListGroupItem>
+        </ListGroup>
+    </Card>
+</section>
   );
 }
