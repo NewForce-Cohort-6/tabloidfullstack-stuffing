caf69f0816336f54129a9bf92ecb268f7fa054a2
Toni Baker <tonijbaker5@gmail.com>
2022-11-22 23:01:07 -0500

    edinging comments

--- Tabloid/client/src/components/views/ApplicationViews.js
+++ Tabloid/client/src/components/views/ApplicationViews.js
@@ -15,7 +15,6 @@ import TagEdit from "../tags/TagEdit";
 import { PostComments } from "../posts/PostComments";
 import { CommentNew } from "../comments/CommentNew";
 import { CommentDelete } from "../comments/CommentDelete";
-
 import { NewPost } from "../posts/PostForm";
 import { CommentEdit } from "../comments/CommentEdit";
 
@@ -43,11 +42,13 @@ export default function ApplicationViews() {
       <Route path ="/my-posts/:id/addComment" element={<CommentNew isMy={true}/>} />
       <Route path ="/posts/:id/addComment" element={<CommentNew isMy={false}/>} />
       <Route path ="/commentDelete/:id" element={<CommentDelete />} />
-
       <Route path="/new-post" element={<NewPost />} />
       <Route path="/addComment" element={<CommentNew />} />
       <Route path ="/commentEdit/:id" element={<CommentEdit />} />
 
+    
+    
+    
     </Routes>
   );
 
