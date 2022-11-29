cef8e389a5615ccc61daaf11d0a768883040e2ae
Toni Baker <tonijbaker5@gmail.com>
2022-11-29 16:02:03 -0500

    toggle activate and deactivate with confirmation

--- Tabloid/client/src/components/users/UserProfileItem.js
+++ Tabloid/client/src/components/users/UserProfileItem.js
@@ -1,17 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, } from "react-router-dom";
 import { getAllUsers, updateToDeactivateUser } from "../../Managers/UserProfileManager";
 import { updateToActivateUser } from "../../Managers/UserProfileManager";
+import { CardLink } from "reactstrap";
+
 
 
 export const UserProfileItem = ({ user, setUserProfile }) => {
     
+    const [confirmDeactivate, setConfirmDeactivate] = useState(false);
+    const [confirmActivate, setConfirmActivate] = useState(false);
+
+
+    
+    const toggleConfirmDeactivate = (e) => {
+        e.preventDefault();
+        setConfirmDeactivate(!confirmDeactivate);
+    };
+    const toggleConfirmActivate = (e) => {
+        e.preventDefault();
+        setConfirmActivate(!confirmActivate);
+    };
     
     const handleDeactivate = () => { // or update IsActive to false
         updateToDeactivateUser(user)
         .then(()=> {
             getAllUsers()
         .then(allUsers => setUserProfile(allUsers))
+        .then(allUsers => setConfirmDeactivate(!confirmDeactivate));
         
         })}
     
@@ -20,6 +37,7 @@ export const UserProfileItem = ({ user, setUserProfile }) => {
         .then(()=> {
             getAllUsers()
             .then(allUsers => setUserProfile(allUsers))
+            .then(allUsers => setConfirmActivate(!confirmActivate));
     })}
     
     
@@ -39,10 +57,59 @@ export const UserProfileItem = ({ user, setUserProfile }) => {
             </td>
             <td>{
                 user.isActive
+                ?<CardLink href={"javascript:void(0)"} onClick={toggleConfirmDeactivate}>
+                Deactivate User
+                </CardLink>
+                :<CardLink href={"javascript:void(0)"} onClick={toggleConfirmActivate}>
+                Activate User
+                </CardLink>
+                }
+
+            </td>
+            {
+                confirmDeactivate ?
+                <div>
+                <h5>Are you sure you want to Deactivate this User?</h5>
+                <button onClick={handleDeactivate} className="btn btn-primary mt-2 mr-5">Deactivate</button>
+                </div>
+                :""
+            }
+            {
+                confirmActivate ?
+                <div>
+                <h5>Are you sure you want to Activate this User?</h5>
+                <button onClick={handleActivate} className="btn btn-primary mt-2 mr-5">Activate</button>
+                </div>
+                :""
+            }
+            
+            {/* {confirmDelete ?
+                    <ListGroup flush>
+                        <ListGroupItem className="text-danger">
+                            Are you sure you want to delete this post?
+                        </ListGroupItem>
+                        <ListGroup flush>
+                            <ListGroupItem>
+                                <Button className="mr-3 btn-danger" onClick={handleDelete}>
+                                    Delete
+                                </Button>
+                                <Button onClick={toggleDeleteConfirm}>
+                                    Cancel
+                                </Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </ListGroup>
+                    : <></>} */}
+            
+            
+            
+            {/* <td>{
+                user.isActive
                 ?<button onClick={handleDeactivate} className="btn btn-primary mt-2 mr-5">Deactivate</button>
+                              
                 :<button onClick={handleActivate} className="btn btn-primary mt-2 mr-5">Activate</button>
             }
-            </td>
+            </td> */}
         </tr>
     )
 }
