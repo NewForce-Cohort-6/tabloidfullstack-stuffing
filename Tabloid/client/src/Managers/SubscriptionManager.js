9deab5ef83677b098a795e7c2d7dfdfa27aacdaf
shanedbutler <shanedbutler@gmail.com>
2022-12-01 16:38:06 -0500

    Fix api call, add function to query subscription for post

--- Tabloid/client/src/Managers/SubscriptionManager.js
+++ Tabloid/client/src/Managers/SubscriptionManager.js
@@ -2,11 +2,21 @@ import { getCurrentUserId } from "./PostManager";
 
 const apiUrl = "https://localhost:5001/api";
 
-// Get subscriptions for the current user
+// Get subscriptions for the current user, currently unsu
 export const getSubscriptions = () => {
     return fetch(`${apiUrl}/Subscription/${getCurrentUserId()}`)
     .then((res) => res.json())
 };
+
+// Get subscription for the current user for a particular post with null response handling
+export const getSubscriptionForPost = (postId) => {
+    return fetch(`${apiUrl}/Subscription/${getCurrentUserId()}/Post/${postId}`)
+    .then((res) => res.text())
+	.then((text) => text.length ? JSON.parse(text) : {})
+	.catch((error) => {
+		throw error;
+	});
+}
 
 // Subscribing means adding a new entry to the Subscription table
 export const subscribeToUser = (body) => {
@@ -21,12 +31,12 @@ export const subscribeToUser = (body) => {
 };
 
 // Un-subscribing means adding an end date value to the Subscription entry
-export const unsubscribeFromUser = (post) => {
-    return fetch(`${apiUrl}/Post/${post.id}`, {
+export const unsubscribeFromUser = (sub) => {
+    return fetch(`${apiUrl}/Subscription/${sub.id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(post)
+        body: JSON.stringify(sub)
       })
 }
